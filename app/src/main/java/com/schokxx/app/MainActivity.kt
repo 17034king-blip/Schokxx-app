119b286ee7870c9d749baec28a47ccad7edc377c
17034king-blip <17034king-blip@users.noreply.github.com>
2026-08-14 02:44:43 +0200

    Fix: Add explicit R import and enable --stacktrace

--- app/src/main/java/com/schokxx/app/MainActivity.kt
+++ app/src/main/java/com/schokxx/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.schokxx.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.schokxx.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
